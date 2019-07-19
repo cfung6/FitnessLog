4d26e0d93383595725704aba08b6ab4eb6c866bf
Chris <chris.fung2@gmail.com>
2019-07-18 20:58:31 -0700

    Added checkboxes for asking how much weight they can do but not their functionality. Also fixed all warnings.

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -18,16 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.new_program);
     }
 
-    public boolean onKeyDown(int keyCode, KeyEvent event)  {
-        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.ECLAIR
-                && keyCode == KeyEvent.KEYCODE_BACK
-                && event.getRepeatCount() == 0) {
-            // Take care of calling this method on earlier versions of
-            // the platform where it doesn't exist.
-            onBackPressed();
-        }
-
-        return super.onKeyDown(keyCode, event);
+    public void levelClicked (View view) {
+        setContentView(R.layout.asking_for_max_weights);
     }
 
     @Override
