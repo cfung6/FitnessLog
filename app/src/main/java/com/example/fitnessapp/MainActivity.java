36f9096dffeb0f70087ca4c38293f235cfc6366e
Chris <chris.fung2@gmail.com>
2019-07-18 19:20:20 -0700

    trying to implement back button

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void newProgramClicked(View view) {
         setContentView(R.layout.new_program);
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.ECLAIR
+                && keyCode == KeyEvent.KEYCODE_BACK
+                && event.getRepeatCount() == 0) {
+            // Take care of calling this method on earlier versions of
+            // the platform where it doesn't exist.
+            onBackPressed();
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // This will be called either automatically for you on 2.0
+        // or later, or by the code above on earlier versions of the
+        // platform.
+        setContentView(R.layout.activity_main);
     }
 }
