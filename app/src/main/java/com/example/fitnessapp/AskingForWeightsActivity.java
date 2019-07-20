c02a9ff5541d42c533c20a9fe80d88b467c5f978
Derek Chen <dchen150@users.noreply.github.com>
2019-07-20 02:55:18 -0700

    Implemented getting input from asking users for weight

--- app/src/main/java/com/example/fitnessapp/AskingForWeightsActivity.java
+++ app/src/main/java/com/example/fitnessapp/AskingForWeightsActivity.java
@@ -4,71 +4,91 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class AskingForWeightsActivity extends AppCompatActivity {
 
+    Button submitButton;
+    EditText benchEditText;
+    EditText overheadPressEditText;
+    EditText squatEditText;
+    EditText deadliftEditText;
+    EditText barbellRowEditText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asking_for_weights);
+        submitButton = (Button)findViewById(R.id.submit_weight_button);
+        benchEditText = (EditText)findViewById(R.id.textbox_bench);
+        overheadPressEditText = (EditText)findViewById(R.id.textbox_overhead);
+        squatEditText = (EditText)findViewById(R.id.textbox_squat);
+        deadliftEditText = (EditText)findViewById(R.id.textbox_deadlift);
+        barbellRowEditText = (EditText)findViewById(R.id.textbox_barbell_row);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String benchInput = benchEditText.getText().toString();
+                String overheadInput = overheadPressEditText.getText().toString();
+                String squatInput = squatEditText.getText().toString();
+                String deadliftInput = squatEditText.getText().toString();
+                String barbellRowInput = barbellRowEditText.getText().toString();
+            }
+        });
 
     }
 
     public void onBenchPressClick (View view) {
-
         boolean checked = ((CheckBox) view).isChecked();
-        EditText editText = findViewById(R.id.textbox_bench);
 
         if (checked) {
-            editText.setVisibility(View.VISIBLE);
+           benchEditText.setVisibility(View.VISIBLE);
         } else {
-            editText.setVisibility(View.GONE);
+            benchEditText.setVisibility(View.GONE);
         }
     }
 
     public void onOverheadPressClick(View view) {
         boolean checked = ((CheckBox) view).isChecked();
-        EditText editText = findViewById(R.id.textbox_overhead);
 
         if (checked) {
-            editText.setVisibility(View.VISIBLE);
+            overheadPressEditText.setVisibility(View.VISIBLE);
         } else {
-            editText.setVisibility(View.GONE);
+            overheadPressEditText.setVisibility(View.GONE);
         }
     }
 
     public void onSquatClick(View view) {
         boolean checked = ((CheckBox) view).isChecked();
-        EditText editText = findViewById(R.id.textbox_squat);
 
         if (checked) {
-            editText.setVisibility(View.VISIBLE);
+            squatEditText.setVisibility(View.VISIBLE);
         } else {
-            editText.setVisibility(View.GONE);
+            squatEditText.setVisibility(View.GONE);
         }
     }
 
     public void onDeadliftClick(View view) {
         boolean checked = ((CheckBox) view).isChecked();
-        EditText editText = findViewById(R.id.textbox_deadlift);
 
         if (checked) {
-            editText.setVisibility(View.VISIBLE);
+            deadliftEditText.setVisibility(View.VISIBLE);
         } else {
-            editText.setVisibility(View.GONE);
+            deadliftEditText.setVisibility(View.GONE);
         }
     }
 
     public void onBarbellRowClick(View view) {
         boolean checked = ((CheckBox) view).isChecked();
-        EditText editText = findViewById(R.id.textbox_barbell_row);
 
         if (checked) {
-            editText.setVisibility(View.VISIBLE);
+            barbellRowEditText.setVisibility(View.VISIBLE);
         } else {
-            editText.setVisibility(View.GONE);
+            barbellRowEditText.setVisibility(View.GONE);
         }
     }
 
