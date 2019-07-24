21de09fc75fe1f9667177a9e4492873180f7bcc1
Chris <chris.fung2@gmail.com>
2019-07-23 23:21:53 -0700

    implemented beginning of file writing to beginner activity

--- app/src/main/java/com/example/fitnessapp/Exercise.java
+++ app/src/main/java/com/example/fitnessapp/Exercise.java
@@ -1,26 +1,33 @@
 package com.example.fitnessapp;
 
+import android.content.Context;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Exercise {
 
 
     private String name;
-    private double weight;
     private Map<Double, List<Integer>> goal;
     private Map<Double, List<Integer>> actual;
+    private Context context;
+    private FileOutputStream fileOutputStream;
 
-    public Exercise (String name) {
+    public Exercise (String name, Context context) {
         this.name = name;
-        this.actual = new HashMap<Double, List<Integer>>();
-        this.goal = new HashMap<Double, List<Integer>>();
+        this.actual = new HashMap<>();
+        this.goal = new HashMap<>();
+        this.context = context;
     }
 
-    public void addWeight(Double weight, List<Integer> reps) {
+    public void addWeight(double weight, List<Integer> reps) {
         this.goal.put(weight, reps);
+        writeToFile(weight, reps);
     }
 
     public String getName () {
@@ -38,9 +45,48 @@ public class Exercise {
 //
 //    }
 
-    public void increaseWeight (int increment) {
+    public void increaseWeight (int increment, double weight, List<Integer> reps) {
         //Read from file
+        double newWeight = weight + increment;
+
+        this.goal.put(weight,reps);
     }
 
+    private void writeToFile (Double weight, List<Integer> reps) {
+
+        String str = this.name.trim().toLowerCase() + " " + weight + " " + listToString(reps) + "\n";
+
+        try {
+            fileOutputStream = this.context.openFileOutput("beginner_data.txt", Context.MODE_PRIVATE);
+            fileOutputStream.write(str.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    private String listToString (List<Integer> reps) {
+
+        String str = "";
+
+        for (int num : reps) {
+
+            str += num + " ";
+        }
+
+        str += "\b";
+
+        return str;
+    }
 
 }
--- app/src/main/java/com/example/fitnessapp/activities/BeginnerActivity.java
+++ app/src/main/java/com/example/fitnessapp/activities/BeginnerActivity.java
@@ -2,6 +2,7 @@ package com.example.fitnessapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,6 +10,12 @@ import android.widget.TextView;
 import com.example.fitnessapp.R;
 import com.example.fitnessapp.Routine;
 import com.example.fitnessapp.Workout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class BeginnerActivity extends AppCompatActivity {
 
@@ -27,6 +34,8 @@ public class BeginnerActivity extends AppCompatActivity {
 //    private Exercise deadlift;
 //    private Exercise barbellRow;
 
+    private FileOutputStream fileOutputStream;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +47,7 @@ public class BeginnerActivity extends AppCompatActivity {
         startingSquatWeight = intent.getDoubleExtra ("SQUAT_WEIGHT", -1);
         startingDeadliftWeight = intent.getDoubleExtra ("DEADLIFT_WEIGHT", -1);
         startingBarbellRowWeight = intent.getDoubleExtra ("BARBELL_ROW_WEIGHT", -1);
+
 
     }
 }
