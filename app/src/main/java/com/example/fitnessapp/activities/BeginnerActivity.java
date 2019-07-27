6e1d53bc79a602d644c42b73d40cda7fdd3ffac4
Chris <chris.fung2@gmail.com>
2019-07-26 23:37:30 -0700

    implemented methods to add exercises to workouts and workouts to routines in beginner activity

--- app/src/main/java/com/example/fitnessapp/activities/BeginnerActivity.java
+++ app/src/main/java/com/example/fitnessapp/activities/BeginnerActivity.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class BeginnerActivity extends AppCompatActivity {
@@ -38,8 +39,6 @@ public class BeginnerActivity extends AppCompatActivity {
     private Exercise deadlift;
     private Exercise barbellRow;
 
-    private FileOutputStream fileOutputStream;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,20 +51,35 @@ public class BeginnerActivity extends AppCompatActivity {
         startingDeadliftWeight = intent.getDoubleExtra ("DEADLIFT_WEIGHT", -1);
         startingBarbellRowWeight = intent.getDoubleExtra ("BARBELL_ROW_WEIGHT", -1);
 
-        benchPress = new Exercise ("benchPress", startingBenchWeight, 5, 1, Arrays.asList(5,5,5));
-//        overheadPress = new Exercise("overheadPress",startingOverheadWeight, 5, 1, Arrays.asList(5,5,5));
-//        squat = new Exercise ("squat", startingSquatWeight, 5, 1, Arrays.asList(5,5,5));
-//        deadlift = new Exercise ("deadlift", startingDeadliftWeight, 5, 1, Arrays.asList(5,5,5));
-//        barbellRow = new Exercise("barbellRow", startingBarbellRowWeight, 5, 1, Arrays.asList(5,5,5));
+        initializeExercises();
+        initializeWorkouts();
+        initializeRoutine();
 
         //For testing
-        benchPress.addRepsDone(75 ,5);
-        benchPress.addRepsDone(75 ,5);
-        benchPress.addRepsDone(70 ,5);
+        benchPress.addRepsDone(100 ,5);
+        benchPress.addRepsDone(100 ,5);
+        benchPress.addRepsDone(100 ,5);
 
         benchPress.increaseWeight();
 
         TextView tv = findViewById(R.id.textView1);
         tv.setText("" + benchPress.getPass() + "\n" + benchPress.getGoalWeight());
+    }
+
+    private void initializeExercises () {
+        benchPress = new Exercise ("benchPress", startingBenchWeight, 5, 1, Arrays.asList(5,5,5));
+        overheadPress = new Exercise("overheadPress",startingOverheadWeight, 5, 1, Arrays.asList(5,5,5));
+        squat = new Exercise ("squat", startingSquatWeight, 5, 1, Arrays.asList(5,5,5));
+        deadlift = new Exercise ("deadlift", startingDeadliftWeight, 5, 1, Arrays.asList(5,5,5));
+        barbellRow = new Exercise("barbellRow", startingBarbellRowWeight, 5, 1, Arrays.asList(5,5,5));
+    }
+
+    private void initializeWorkouts () {
+        workoutA = new Workout ("workoutA", new HashSet<>(Arrays.asList(squat,benchPress,barbellRow)));
+        workoutB = new Workout("workoutB", new HashSet<>(Arrays.asList(squat,overheadPress,deadlift)));
+    }
+
+    private void initializeRoutine () {
+        beginnerRoutine = new Routine(3, "beginner_routine", new HashSet<>(Arrays.asList(workoutA,workoutB)));
     }
 }
