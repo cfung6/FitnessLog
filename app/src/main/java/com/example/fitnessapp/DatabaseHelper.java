a1f4348b5161ef6f6ea00c32c435fb6fde9510a1
Chris <chris.fung2@gmail.com>
2019-08-12 01:57:52 -0700

    tried normalizing tables

--- app/src/main/java/com/example/fitnessapp/DatabaseHelper.java
+++ app/src/main/java/com/example/fitnessapp/DatabaseHelper.java
@@ -7,15 +7,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "fitness.db";
-    private static final String TABLE_NAME = "fitness_table";
-    private static final String COL1 = "ID";
-    private static final String COL2 = "Name";
-    private static final String COL3 = "Date";
-    private static final String COL4 = "Routine";
-    private static final String COL5 = "Workout";
-    private static final String COL6 = "Weight";
-    private static final String COL7 = "Sets";
-    private static final String COL8 = "Reps";
+    private static final String ROUTINE_TABLE = "routine_table";
+    private static final String BEGINNER_TABLE = "beginner_table";
+    private static final String INTERMEDIATE_TABLE = "intermediate_table";
+    private static final String ADVANCED_TABLE = "advanced_table";
+    private static final String EXERCISE_TABLE = "exercise_table";
+    private static final String EXERCISE_NAME_COL = "Exercise_name";
+    private static final String DATE_COL = "Date";
+    private static final String ROUTINE_ID = "Routine_ID";
+    private static final String ROUTINE_NAME_COL = "Routine_name";
+    private static final String EXERCISE_ID = "Exercise_ID";
+    private static final String WORKOUT_ID = "Workout_ID";
+    private static final String WEIGHT_COL = "Weight";
+    private static final String SETS_COL = "Sets";
+    private static final String REPS_COL = "Reps";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -23,20 +28,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME
-                + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + COL2 + " TEXT, "
-                + COL3 + " INTEGER, "
-                + COL4 + " TEXT, "
-                + COL5 + " TEXT, "
-                + COL6 + " REAL, "
-                + COL7 + " INTEGER, "
-                + COL8 + " INTEGER)");
+        db.execSQL("CREATE TABLE " + ROUTINE_TABLE
+                + " (" + ROUTINE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ROUTINE_NAME_COL + " TEXT)");
+
+        db.execSQL("CREATE TABLE " + BEGINNER_TABLE
+        );
+
+        db.execSQL("CREATE TABLE " + EXERCISE_TABLE
+                + " (" + EXERCISE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + EXERCISE_NAME_COL + " TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + EXERCISE_TABLE);
         onCreate(db);
     }
 
