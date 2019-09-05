2fe99cd3f1279dcaff28c8b0d9cc2ce5cf1cbc21
Chris <chris.fung2@gmail.com>
2019-09-04 23:21:55 -0700

    Finished writing the routine descriptions in the About activity

--- app/src/main/java/com/example/fitnesslog/activities/AboutActivity.java
+++ app/src/main/java/com/example/fitnesslog/activities/AboutActivity.java
@@ -15,16 +15,37 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         TextView tv = findViewById(R.id.message);
-        String message = "Fitness App is an application that generates and tracks workouts. " +
+        String message = "FitnessLog is an application that generates and tracks workouts. " +
                 "First, you select a level (beginner, intermediate, or advanced) " +
                 "based on your experience in the gym as well as your availability to go to the gym. " +
                 "Next, you will be prompted for the weight " +
                 "that you are able to lift for certain exercises. " +
-                "Workouts will then be generated for you that target major muscle groups of " +
-                "the body setting a goal for you to lift, as well as the number of " +
+                "This app will generate workouts and " +
+                "set a goal weight for you to lift, as well as the number of " +
                 "sets and reps that you should try to attempt. Your following workouts will " +
                 "be determined based on how you did in the current workout. Workouts will be stored in our " +
                 "calendar tool, where you can check and track your progress!";
+        tv.setText(message);
+
+        tv = findViewById(R.id.beginner_message);
+        message = "This program is a three times per week routine that focuses on linear progression" +
+                " using the five main compound exercises. After successfully completing the necessary weight" +
+                " and reps for each exercise, increase the weight by 5 pounds the next time you have to do the" +
+                " exercise.";
+        tv.setText(message);
+
+        tv = findViewById(R.id.intermediate_message);
+        message = "This intermediate program is done three times per week. The first day of the week focuses on volume," +
+                " requiring 5 sets of 5 for squat and either bench or overhead press (using 90 percent of 5 rep max)." +
+                " The second workout, the light workout, contains squat and bench/overhead press (whichever one was not done" +
+                " in the first workout) using 81 percent of your 5 rep max. The last workout, the intensity day, is when" +
+                " you try to achieve a new 5 rep max in squat and the exercise done in the first workout.";
+        tv.setText(message);
+
+        tv = findViewById(R.id.advanced_message);
+        message = "This advanced program focuses on volume and hypertrophy and should be done four to six times a week." +
+                " This routine contains more than triple the exercises of the other two routines." +
+                " After each exercise is completed successfully, five pounds is increased to that exercise.";
         tv.setText(message);
     }
 }
